7b939f1505676f76f12cad9b763169f92ca2b674
maryane <aldenoramaryanne@gmail.com>
2022-10-31 08:45:32 -0300

    first commit

---
+++ Static/js/introducao.js
@@ -0,0 +1,91 @@
+// //Comando que exibe informações no console
+// //console.log("Hello World")
+
+
+// //Variáveis e tipos de dados
+// //string
+// let nome = "MaryMine"
+// console.log(nome)
+
+// //number
+// let idade = 19
+// console.log(idade)
+
+// //boolean
+// let programadora = true
+// console.log(programadora)
+
+//operações matemáticas
+// let num1 = 3
+// let num2 = 5
+
+// console.log("Soma")
+// console.log(num1 + num2)
+// // Subtração
+// console.log("Subtração")
+// console.log(num1 - num2)
+//  // Multiplicação
+// console.log("Multiplicação")
+// console.log(num1 * num2)
+// // Divisão
+// console.log("Divisão")
+// console.log(num1 / num2)
+
+// // Módulo ou resto da divisão
+// console.log("Módulo") 
+// console.log(num1 % num2)
+
+//arrays --> guardar dentro de 1 variável, várias informações -> estrutura de dados
+
+// let notas = [10, 1, 8.9]
+// console.log(notas)
+// console.log(notas[2])
+
+// notas[0] = 8.5
+// console.log(notas)
+
+// // métodos -> funções a mais que vc está adicionando a uma variável
+
+// notas.push(9.1)
+// console.log(notas)
+
+//laços de repetição 
+
+// let notas = [10, 8, 8.9]
+
+// //for tradicional
+
+// for(let i = 0; i < 4; i++){
+//     console.log(notas[i])
+// }
+
+
+// //for of
+
+// for(let nota of notas) {
+//     console.log(nota);
+// }
+// console.log("Terminou o for of")
+
+
+// Objetos JavaScript
+// let aluno = {
+//     nome: "Mary",
+//     idade: 19,
+//     notas: [10, 8.9, 7.3, 8.5],
+//     contato: {
+//         telefone: "(11) 99999-9999",
+//         email: "mary@mail.com"
+//     }
+// }
+// console.log(aluno)
+// console. log(aluno.nome)
+// console. log (aluno.notas[0])
+// console. log(aluno.contato.email)
+// console. log(aluno.contato.telefone)
+
+// //Manipulando o DOM 
+// let elemento = document.getElementById("Titulo")
+// console.log(elemento)
+
+// elemento.innerHTML = "Somos todos Humano"
